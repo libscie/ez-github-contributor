c5cce1dd98e1af6adc0aea138243826d63187bb6
Chris Hartgerink <chris@libscie.org>
2024-04-14 15:48:41 +0200

    Test netlify function

---
+++ netlify/functions/test.js
@@ -0,0 +1,19 @@
+// netlify/functions/postHandler.js
+exports.handler = async function(event, context) {
+    if (event.httpMethod !== 'POST') {
+      return {
+        statusCode: 405,
+        body: JSON.stringify({ message: "Method Not Allowed" }),
+        headers: { 'Allow': 'POST' }
+      };
+    }
+  
+    const data = JSON.parse(event.body);
+    const name = data.name || 'World';
+  
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `Hello ${name}!` })
+    };
+  };
+  
