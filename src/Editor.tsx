d5370b01561a199a03a74c784b9ceaf4661550b0
Chris Hartgerink <chris@libscie.org>
2024-04-21 15:35:01 +0200

    Add Tailwind

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -69,9 +69,12 @@ function Editor() {
         <OnChangePlugin onChange={onChange} />
       </div>
     </div>
-    <button formMethod='POST' formTarget='/.netlify/functions/test'>
-      Submit
-    </button>
+    <button
+        type="button"
+        className="rounded bg-indigo-600 px-2 py-1 text-base font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      >
+        Submit
+      </button>
   </LexicalComposer>
   );
 }
